5ce72aef5ab1fded5e5d81dec28070dd5ec6ffa0
gabe <gabeklav@fastmail.com>
2024-04-19 10:13:52 -0400

    turn off change detection notification

--- init.lua
+++ init.lua
@@ -14,4 +14,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("plugins")
+require("lazy").setup({
+  spec = "plugins",
+  change_detection = { notify = false }
+})
