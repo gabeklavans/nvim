9740e24a084edc225e009b293b8d852f441c7fc9
gabe <gabeklav@fastmail.com>
2024-04-19 10:44:33 -0400

    add lualine

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,37 @@
+local function codestats()
+    -- return "C::S ".. require('codestats-nvim').get_xp_count()
+    return "C::S"
+end
+
+return {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    opts = {
+        options = {
+            icons_enabled = true,
+            theme = 'auto',
+        },
+        sections = {
+            lualine_a = { 'mode' },
+            lualine_b = { 'branch', 'diff', 'diagnostics' },
+            lualine_c = {
+                {
+                    'filename',
+                    file_status = true,
+                    newfile_status = true,
+                    path = 1,
+                },
+            },
+            lualine_x = {
+                'fileformat',
+                {
+                    'filetype',
+                    icon_only = true,
+                },
+                codestats },
+            lualine_y = { 'progress' },
+            lualine_z = { 'location' }
+        },
+    },
+
+}
