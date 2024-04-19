41627645a47f2c87f76da3fa74f1226677149c3e
gabe <gabeklav@fastmail.com>
2024-04-19 11:10:05 -0400

    add conform.nvim

---
+++ lua/plugins/format.lua
@@ -0,0 +1,26 @@
+return {
+    "stevearc/conform.nvim",
+    event = { "BufWritePre" },
+    cmd = { "ConformInfo" },
+    keys = {
+        {
+            "<leader>f",
+            function()
+                require("conform").format({ async = true, lsp_fallback = true })
+            end,
+            mode = "",
+            desc = "[F]ormat buffer",
+        },
+    },
+    opts = {
+        formatters_by_ft = {
+            lua = { "stylua" },
+            python = { "isort", "black" },
+            javascript = { { "prettierd", "prettier" } },
+        },
+    },
+    init = function()
+        vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+    end,
+}
+
