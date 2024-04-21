f0cf12432e7b45bea835204517cf9b9ce32fc9aa
gabe <gabeklav@fastmail.com>
2024-04-21 19:07:45 -0400

    add some old and relevant which-key chains

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -38,10 +38,10 @@ return {
             map('n', '<leader>hR', gitsigns.reset_buffer, { desc = "Buff[h]er [R]eset" })
             map('n', '<leader>hp', gitsigns.preview_hunk, { desc = "[H]unk [P]review" })
             map('n', '<leader>hb', function() gitsigns.blame_line { full = true } end, { desc = "[H]uh? [B]lame Line" })
-            map('n', '<leader>ht', gitsigns.toggle_current_line_blame, { desc = "[H]uh? [T]oggle Inline Blame" })
+            map('n', '<leader>tb', gitsigns.toggle_current_line_blame, { desc = "[T]oggle Inline [B]lame" })
             map('n', '<leader>hd', gitsigns.diffthis, { desc = "Buff[h]er [D]iff" })
             -- map('n', '<leader>hD', function() gitsigns.diffthis('~') end)
-            map('n', '<leader>td', gitsigns.toggle_deleted)
+            map('n', '<leader>td', gitsigns.toggle_deleted, { desc = "[T]oggle [D]eleted" })
         end
     }
 }
