7924ee4813ad76efdb35c8551d80e7a4a3a5eba5
gabe <gabriel.k@yobeinc.com>
2024-04-26 10:59:12 -0400

    enable highlight on yank

--- lua/set.lua
+++ lua/set.lua
@@ -27,3 +27,10 @@ vim.opt.isfname:append("@-@")
 vim.opt.updatetime = 50
 
 vim.opt.colorcolumn = "80"
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+    pattern = '*',
+    callback = function ()
+        vim.highlight.on_yank {}
+    end,
+})
