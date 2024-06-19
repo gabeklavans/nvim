d57353c51fa214fc00698518d20ffa46a4b5049d
gabe <gabeklav@fastmail.com>
2024-06-19 09:20:37 -0400

    add new opinionated defaults to telescope live grep

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,10 +1,14 @@
 return {
     "nvim-telescope/telescope.nvim",
     branch = "0.1.x",
-    dependencies = { "nvim-lua/plenary.nvim" },
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "fdschmidt93/telescope-egrepify.nvim"
+    },
     config = function()
+        local telescope = require "telescope"
         -- this has to go in here instead of in `opts` for some reason
-        require("telescope").setup {
+        telescope.setup {
             pickers = {
                 live_grep = {
                     additional_args = function(opts)
@@ -13,11 +17,12 @@ return {
                 },
             },
         }
+        telescope.load_extension "egrepify"
 
         local builtin = require("telescope.builtin")
 
         local function telescope_live_grep_open_files()
-            require('telescope.builtin').live_grep {
+            builtin.live_grep {
                 grep_open_files = true,
                 prompt_title = 'Live Grep in Open Files',
             }
@@ -29,7 +34,7 @@ return {
         vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
         vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
         vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
-        vim.keymap.set("n", "<leader>si", builtin.live_grep, { desc = "[S]earch [i]n Files" })
+        vim.keymap.set("n", "<leader>si", telescope.extensions.egrepify.egrepify, { desc = "[S]earch [i]n Files" })
         vim.keymap.set("n", "<leader>sG", ":LiveGrepGitRoot<cr>", { desc = "[S]earch by [G]rep on Git Root" })
         vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
         vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
