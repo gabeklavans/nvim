dd9d7f6068c077400e1bac9afc35a7f5c6b4baaa
gabe <gabeklav@fastmail.com>
2024-06-26 13:01:54 -0400

    turn on list mode
    also turn off `expandtab`; I cannot presume to control the code style of
    all codebases I touch

--- lua/set.lua
+++ lua/set.lua
@@ -4,11 +4,14 @@ vim.opt.relativenumber = true
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
+vim.opt.expandtab = false
 
 vim.opt.smartindent = true
 
-vim.opt.wrap = false
+vim.opt.wrap = true
+
+-- I will not be caught off-guard by mixed spaces/tabs!!!
+vim.opt.list = true
 
 -- Windows skill issue
 -- vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
@@ -29,5 +32,3 @@ vim.opt.isfname:append("@-@")
 vim.opt.updatetime = 50
 
 vim.opt.colorcolumn = "80"
-
-vim.opt.wrap = true
