f68f360c1f9b47f343f73e0c61770042f107e850
gabe <gabeklav@fastmail.com>
2024-08-02 10:47:53 -0400

    formate

--- lua/remaps.lua
+++ lua/remaps.lua
@@ -1,8 +1,7 @@
 vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>oe", vim.cmd.Ex, { desc = "[O]pen N[e]twr" })
-
-vim.keymap.set('n', "<leader>n", ":nohl<CR>", { desc = "[N]o Highlighting"})
+vim.keymap.set('n', "<leader>n", ":nohl<CR>", { desc = "[N]o Highlighting" })
 
 -- keep cursor in center of the screen
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
@@ -11,19 +10,19 @@ vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
 -- thanks asbjornHaland
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]], { desc = "[Y]ank to system clipboard" })
-vim.keymap.set("n", "<leader>Y", [["+Y]], { desc = "[Y]ank-default to system clipboard "})
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = "[Y]ank to system clipboard" })
+vim.keymap.set("n", "<leader>Y", [["+Y]], { desc = "[Y]ank-default to system clipboard " })
 
 -- navigate quickfix list
 vim.keymap.set("n", "<C-j>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-k>", "<cmd>cprev<CR>zz")
-vim.keymap.set("n", "<leader>j", "<cmd>lnext<CR>zz", { desc = "Relative next quickfix"})
-vim.keymap.set("n", "<leader>k", "<cmd>lprev<CR>zz", { desc = "Relative previous quickfix"})
+vim.keymap.set("n", "<leader>j", "<cmd>lnext<CR>zz", { desc = "Relative next quickfix" })
+vim.keymap.set("n", "<leader>k", "<cmd>lprev<CR>zz", { desc = "Relative previous quickfix" })
 
 -- always be tweaking the config...
-vim.keymap.set("n", "<leader>nec", "<cmd>e ~/.config/nvim<CR>", { desc = "[N]eovim [E]dit [C]onfig"});
+vim.keymap.set("n", "<leader>nec", "<cmd>e ~/.config/nvim<CR>", { desc = "[N]eovim [E]dit [C]onfig" });
 
--- splits 
+-- splits
 vim.keymap.set('n', '<ca-h>', '<C-w>h', { silent = true })
 vim.keymap.set('n', '<ca-j>', '<C-w>j', { silent = true })
 vim.keymap.set('n', '<ca-k>', '<C-w>k', { silent = true })
