b617f12cbacfd7d001555d7558c55650f8644580
gabe <gabeklav@fastmail.com>
2024-08-12 10:45:15 -0400

    add quicker.nvim for nifty quickfix list features/highlighting

---
+++ lua/plugins/quicker.lua
@@ -0,0 +1,32 @@
+return {
+    'stevearc/quicker.nvim',
+    event = "FileType qf",
+    ---@module "quicker"
+    ---@type quicker.SetupOptions
+    opts = {
+        keys = {
+            {
+                ">",
+                function()
+                    require("quicker").expand({ before = 2, after = 2, add_to_existing = true })
+                end,
+                desc = "Expand quickfix context",
+            },
+            {
+                "<",
+                function()
+                    require("quicker").collapse()
+                end,
+                desc = "Collapse quickfix context",
+            },
+        },
+
+    },
+    init = function()
+        vim.keymap.set("n", "<leader>qf", function()
+            require("quicker").toggle()
+        end, {
+            desc = "Toggle [Q]uick[f]ix list",
+        })
+    end,
+}
