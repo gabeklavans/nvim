ae8aa4f06f3b442b116178aa9de94898f6c9113f
gabe <gabeklav@fastmail.com>
2024-08-12 10:24:31 -0400

    organize remaps better

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -7,8 +7,9 @@ return {
 
         harpoon:setup()
 
-        vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
-        vim.keymap.set("n", "<leader>q", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+        vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end, { desc = "[A]dd to Harpoon list" })
+        vim.keymap.set("n", "<leader>qh", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end,
+            { desc = "Toggle [Q]uick [H]arpoon Menu" })
 
         vim.keymap.set("n", "<C-j>", function() harpoon:list():select(1) end)
         vim.keymap.set("n", "<C-k>", function() harpoon:list():select(2) end)
--- lua/remaps.lua
+++ lua/remaps.lua
@@ -14,8 +14,8 @@ vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = "[Y]ank to system cl
 vim.keymap.set("n", "<leader>Y", [["+Y]], { desc = "[Y]ank-default to system clipboard " })
 
 -- navigate quickfix list
-vim.keymap.set("n", "<C-j>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-k>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<CS-J>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<CS-K>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lnext<CR>zz", { desc = "Relative next quickfix" })
 vim.keymap.set("n", "<leader>k", "<cmd>lprev<CR>zz", { desc = "Relative previous quickfix" })
 
@@ -23,12 +23,12 @@ vim.keymap.set("n", "<leader>k", "<cmd>lprev<CR>zz", { desc = "Relative previous
 vim.keymap.set("n", "<leader>nec", "<cmd>e ~/.config/nvim<CR>", { desc = "[N]eovim [E]dit [C]onfig" });
 
 -- splits
-vim.keymap.set('n', '<ca-h>', '<C-w>h', { silent = true })
-vim.keymap.set('n', '<ca-j>', '<C-w>j', { silent = true })
-vim.keymap.set('n', '<ca-k>', '<C-w>k', { silent = true })
-vim.keymap.set('n', '<ca-l>', '<C-w>l', { silent = true })
+vim.keymap.set('n', '<CA-h>', '<C-w>h', { silent = true })
+vim.keymap.set('n', '<CA-j>', '<C-w>j', { silent = true })
+vim.keymap.set('n', '<CA-k>', '<C-w>k', { silent = true })
+vim.keymap.set('n', '<CA-l>', '<C-w>l', { silent = true })
 
--- Remap for dealing with word wrap
+-- dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
