43e9e43136a2955689d6745d226816c134bd6966
gabe <gabeklav@fastmail.com>
2024-08-14 10:52:42 -0400

    add TS context toggle remap

--- lua/plugins/context.lua
+++ lua/plugins/context.lua
@@ -1,3 +1,10 @@
 return {
   "nvim-treesitter/nvim-treesitter-context",
+  init = function()
+    vim.keymap.set("n", "<leader>tc", function()
+      require("treesitter-context").toggle()
+    end, {
+      desc = "[T]oggle TS-[C]ontext",
+    })
+  end,
 }
