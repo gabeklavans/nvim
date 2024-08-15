7db46e7a0b3653dba3372d4fab8449a7dd029f94
gabe <gabeklav@fastmail.com>
2024-08-14 23:15:53 -0400

    update which key config

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -5,19 +5,15 @@ return {
         vim.o.timeout = true
         vim.o.timeoutlen = 300
     end,
-    opts = {},
-    config = function()
-        require('which-key').register {
-            ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-            -- ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-            ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
-            ['<leader>h'] = { name = 'Gitsigns/[H]unk', _ = 'which_key_ignore' },
-            -- ['<leader>a'] = { name = 'H[a]rpoon', _ = 'which_key_ignore' },
-            ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-            ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-            ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-            ['<leader>q'] = { name = '[Q]uick views', _ = 'which_key_ignore' },
-            -- ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+    opts = {
+        spec = {
+            { '<leader>c', desc = 'Code' },
+            { '<leader>g', desc = 'Git' },
+            { '<leader>h', desc = 'Gitsigns/Hunk' },
+            { '<leader>r', desc = 'Rename' },
+            { '<leader>s', desc = 'Search' },
+            { '<leader>t', desc = 'Toggle' },
+            { '<leader>q', desc = 'Quick views' },
         }
-    end
+    },
 }
