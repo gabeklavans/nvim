a3473ab0ec9cabe639c51d6249ebe754c32cf76e
Gabe Klavans <gbk@ibm.com>
2024-09-03 13:32:53 -0400

    switch theme to catppuccin

--- lua/plugins/color.lua
+++ lua/plugins/color.lua
@@ -1,7 +1,42 @@
-return {
-    'Mofiqul/dracula.nvim',
+return
+-- {
+--     'Mofiqul/dracula.nvim',
+--     priority = 1000,
+--     config = function()
+--         vim.cmd.colorscheme 'dracula'
+--     end,
+-- }
+-- {
+--     'Mofiqul/vscode.nvim',
+--     lazy = false,
+--     priority = 1000,
+--     config = function()
+--         require('vscode').setup {
+--             transparent = false,
+--         }
+--         vim.cmd.colorscheme "vscode"
+--     end,
+-- }
+-- {
+--     "folke/tokyonight.nvim",
+--     lazy = false,
+--     priority = 1000,
+--     config = function()
+--         require("tokyonight").setup {
+--             style = "night"
+--         };
+--         vim.cmd [[colorscheme tokyonight]]
+--     end
+-- }
+{
+    "catppuccin/nvim",
+    name = "catppuccin",
     priority = 1000,
     config = function()
-        vim.cmd.colorscheme 'dracula'
-    end,
+        require("catppuccin").setup {
+            flavour = "auto",
+        }
+
+        vim.cmd.colorscheme "catppuccin"
+    end
 }
