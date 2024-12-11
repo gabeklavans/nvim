d357d4c49fac7bf1dcecc72fd6ebb0e594e33f1a
Gabe Klavans <gbk@ibm.com>
2024-12-11 09:59:59 -0500

    add some display toggles for sharing code

--- lua/remaps.lua
+++ lua/remaps.lua
@@ -20,3 +20,35 @@ vim.keymap.set('n', '<CA-l>', '<C-w>l', { silent = true })
 
 -- <C-^> is inconsistent and hard to reach
 vim.keymap.set('n', '<C-t>', '<C-^>', { silent = true })
+
+-- for copy-paste
+local is_share_mode = false
+local function toggle_share_mode()
+    is_share_mode = not is_share_mode
+    if is_share_mode then
+        vim.opt.relativenumber = false
+        vim.opt.number = false
+        vim.opt.list = false
+    else
+        vim.opt.relativenumber = true
+        vim.opt.number = true
+        vim.opt.list = true
+    end
+end
+vim.keymap.set('n', '<leader>sm', toggle_share_mode, { desc = "toggle share mode "})
+
+-- for displaying screen to others
+local is_display_mode = false
+local function toggle_display_mode()
+    is_display_mode = not is_display_mode
+    if is_display_mode then
+        vim.opt.relativenumber = false
+        vim.opt.number = true
+        vim.opt.list = false
+    else
+        vim.opt.relativenumber = true
+        vim.opt.number = true
+        vim.opt.list = true
+    end
+end
+vim.keymap.set('n', '<leader>dm', toggle_display_mode, { desc = "toggle display mode "})
