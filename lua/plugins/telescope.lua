868eea770ce7d0a17fecbfbca15a891e1f829718
Gabriel Klavans <gbk@ibm.com>
2025-01-10 10:36:52 -0500

    disable file preview on telescope file picker

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -15,6 +15,9 @@ return {
                         return { "--hidden" }
                     end
                 },
+                find_files = {
+                    previewer = false,
+                }
             },
             extensions = {
                 egrepify = {
