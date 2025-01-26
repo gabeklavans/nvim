395986c997e4fbd45060fe5d4e12725fcbf0263b
gabe <gabeklav@fastmail.com>
2025-01-25 21:56:28 -0500

    fix out of date conform config

--- lua/plugins/format.lua
+++ lua/plugins/format.lua
@@ -16,8 +16,9 @@ return {
         formatters_by_ft = {
             lua = { "stylua" },
             python = { "isort", "black" },
-            javascript = { { "prettierd", "prettier" } },
-            typescript = { { "prettierd", "prettier" } },
+            javascript = { "prettierd", "prettier", stop_after_first = true },
+            typescript = { "prettierd", "prettier", stop_after_first = true },
+            html = { "prettierd", "prettier", stop_after_first = true }
         },
     },
     init = function()
