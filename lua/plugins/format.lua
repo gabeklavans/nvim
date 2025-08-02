cc9e5c4aa6e07bfccc93ad1ede1ab187b5285994
gabe <gabeklav@fastmail.com>
2025-08-02 00:21:08 -0400

    lint/format improvements

--- lua/plugins/format.lua
+++ lua/plugins/format.lua
@@ -1,28 +1,27 @@
 return {
-    "stevearc/conform.nvim",
-    event = { "BufWritePre" },
-    cmd = { "ConformInfo" },
-    keys = {
-        {
-            "<leader>f",
-            function()
-                require("conform").format({ async = true, lsp_fallback = true })
-            end,
-            mode = "",
-            desc = "Format buffer",
-        },
-    },
-    opts = {
-        formatters_by_ft = {
-            lua = { "stylua" },
-            python = { "isort", "black" },
-            javascript = { "prettierd", "prettier", stop_after_first = true },
-            typescript = { "prettierd", "prettier", stop_after_first = true },
-            html = { "prettierd", "prettier", stop_after_first = true },
-            markdown = { "mdformat", stop_after_first = true },
-        },
-    },
-    init = function()
-        vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
-    end,
+	"stevearc/conform.nvim",
+	cmd = { "ConformInfo" },
+	keys = {
+		{
+			"<leader>f",
+			function()
+				require("conform").format({ async = true, lsp_fallback = true })
+			end,
+			mode = "",
+			desc = "Format buffer",
+		},
+	},
+	opts = {
+		formatters_by_ft = {
+			lua = { "stylua" },
+			python = { "autopep8", "isort" },
+			javascript = { "prettierd", "prettier", stop_after_first = true },
+			typescript = { "prettierd", "prettier", stop_after_first = true },
+			html = { "prettierd", "prettier", stop_after_first = true },
+			markdown = { "mdformat", stop_after_first = true },
+		},
+	},
+	init = function()
+		vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+	end,
 }
--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -1,26 +1,20 @@
 return {
-    "mfussenegger/nvim-lint",
-    event = {
-        "BufWritePost",
-    },
-    keys = {
-        "<leader>t",
-    },
-    config = function()
-        local lint = require("lint")
+	"mfussenegger/nvim-lint",
+	event = {
+		"BufWritePost",
+	},
+	keys = {
+		"<leader>t",
+	},
+	config = function()
+		local lint = require("lint")
 
-        lint.linters_by_ft = {
-            python = { "pylint" },
-        }
+		lint.linters_by_ft = {
+			python = { "pylint" },
+		}
 
-        vim.keymap.set("n", "<leader>t", function()
-            lint.try_lint()
-        end, { desc = "lint file" })
-
-        vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-            callback = function()
-                lint.try_lint()
-            end
-        })
-    end,
+		vim.keymap.set("n", "<leader>t", function()
+			lint.try_lint()
+		end, { desc = "lint file" })
+	end,
 }
