28a026be1f69f1702318bc848846890557cccf3c
gabe <gabeklav@fastmail.com>
2025-10-14 12:33:57 -0400

    better autopair behavior when inserting before a character

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,5 +1,21 @@
 return {
-    'echasnovski/mini.pairs',
-    version = '*',
-    opts = {},
+	"echasnovski/mini.pairs",
+	version = "*",
+	opts = {
+		-- thanks https://github.com/Sarctiann/lazyvim_config/blob/main/lua/plugins/mini-pairs.lua
+		mappings = {
+			-- Prevents the action if the cursor is just before any character or next to a "\".
+			["("] = { action = "open", pair = "()", neigh_pattern = "[^\\][%s%)%]%}]" },
+			["["] = { action = "open", pair = "[]", neigh_pattern = "[^\\][%s%)%]%}]" },
+			["{"] = { action = "open", pair = "{}", neigh_pattern = "[^\\][%s%)%]%}]" },
+			-- This is default (prevents the action if the cursor is just next to a "\").
+			[")"] = { action = "close", pair = "()", neigh_pattern = "[^\\]." },
+			["]"] = { action = "close", pair = "[]", neigh_pattern = "[^\\]." },
+			["}"] = { action = "close", pair = "{}", neigh_pattern = "[^\\]." },
+			-- Prevents the action if the cursor is just before or next to any character.
+			['"'] = { action = "closeopen", pair = '""', neigh_pattern = "[^%w][^%w]", register = { cr = false } },
+			["'"] = { action = "closeopen", pair = "''", neigh_pattern = "[^%w][^%w]", register = { cr = false } },
+			["`"] = { action = "closeopen", pair = "``", neigh_pattern = "[^%w][^%w]", register = { cr = false } },
+		},
+	},
 }
