f1bc0c21b3af7f6d7ae03e32199f1d15cb1168b8
gabe <gabeklav@fastmail.com>
2025-12-23 16:37:34 -0500

    switch to mini.completion for autocomplete
    the built-in was just too buggy with sorting and autotriggers

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,28 +3,30 @@ return {
 	dependencies = {
 		"mason-org/mason.nvim",
 		"mason-org/mason-lspconfig.nvim",
+		{ "nvim-mini/mini.completion", version = "*" },
 	},
 	config = function()
 		require("mason").setup()
 		require("mason-lspconfig").setup()
+		require('mini.completion').setup()
 
-		vim.api.nvim_create_autocmd("LspAttach", {
-			group = vim.api.nvim_create_augroup("my.lsp", {}),
-			callback = function(args)
-				local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
-				if client:supports_method("textDocument/completion") then
-					-- trigger auto-complete on every alpha character
-					local chars = {}
-					for i = 65, 122 do
-						table.insert(chars, string.char(i))
-					end
-					table.insert(chars, ".")
-					table.insert(chars, ">")
-					client.server_capabilities.completionProvider.triggerCharacters = chars
-
-					vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
-				end
-			end,
-		})
+		-- vim.api.nvim_create_autocmd("LspAttach", {
+		-- 	group = vim.api.nvim_create_augroup("my.lsp", {}),
+		-- 	callback = function(args)
+		-- 		local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+		-- 		if client:supports_method("textDocument/completion") then
+		-- 			-- trigger auto-complete on every alpha character
+		-- 			local chars = {}
+		-- 			for i = 65, 122 do
+		-- 				table.insert(chars, string.char(i))
+		-- 			end
+		-- 			table.insert(chars, ".")
+		-- 			table.insert(chars, ">")
+		-- 			client.server_capabilities.completionProvider.triggerCharacters = chars
+		--
+		-- 			vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = false })
+		-- 		end
+		-- 	end,
+		-- })
 	end,
 }
