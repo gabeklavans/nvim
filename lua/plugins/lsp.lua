e6593f7dc0fa46f27786e2662b6560a0fef804b4
gabe <gabeklav@fastmail.com>
2025-12-24 15:19:34 -0500

    remove old comment

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -9,24 +9,5 @@ return {
 		require("mason").setup()
 		require("mason-lspconfig").setup()
 		require('mini.completion').setup()
-
-		-- vim.api.nvim_create_autocmd("LspAttach", {
-		-- 	group = vim.api.nvim_create_augroup("my.lsp", {}),
-		-- 	callback = function(args)
-		-- 		local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
-		-- 		if client:supports_method("textDocument/completion") then
-		-- 			-- trigger auto-complete on every alpha character
-		-- 			local chars = {}
-		-- 			for i = 65, 122 do
-		-- 				table.insert(chars, string.char(i))
-		-- 			end
-		-- 			table.insert(chars, ".")
-		-- 			table.insert(chars, ">")
-		-- 			client.server_capabilities.completionProvider.triggerCharacters = chars
-		--
-		-- 			vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = false })
-		-- 		end
-		-- 	end,
-		-- })
 	end,
 }
